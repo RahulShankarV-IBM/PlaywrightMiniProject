e28bd43a96729cfb910173149a28ae7f7b1ca5a0
Rahul Shankar <rahulshankar@ibm.com>
2026-09-20 09:11:52 +0530

    Added a package for better reporting

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,11 @@ module.exports = {
     default: {
         paths:   ['tests/features/**/*.feature'],
         require: ['tests/support/**/*.js', 'tests/steps/**/*.js'],
-        format:  ['progress-bar', 'html:reports/cucumber-report.html'],
+        format:  [
+            'progress-bar',
+            'json:reports/cucumber-report.json',
+            'html:reports/cucumber-report.html'
+        ],
         parallel: 0
     }
 };
